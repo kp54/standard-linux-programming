0ccbb5c66ec4fb9f35d063f8d482c207838b0014
kp54 <kangpang65@gmail.com>
2021-08-30 20:20:54 +0900

    rewrite

--- head/head.c
+++ head/head.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-static void do_head(FILE* f, long nlines);
 
-int
-main(int argc, char *argv[])
+static void do_head(FILE *f, long nlines)
 {
-    if (argc != 2) {
+    if (nlines <= 0)
+        return;
+
+    int c;
+    while (true)
+    {
+        c = getc(f);
+        if (c == EOF)
+            return;
+
+        if (putchar(c) < 0)
+            exit(EXIT_FAILURE);
+
+        if (c == '\n')
+        {
+            nlines--;
+
+            if (nlines == 0)
+                return;
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s n\n", argv[0]);
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
     do_head(stdin, atol(argv[1]));
-    exit(0);
-}
 
-static void
-do_head(FILE *f, long nlines)
-{
-    int c;
-
-    if (nlines <= 0) return;
-    while ((c = getc(f)) != EOF) {
-        if (putchar(c) < 0) exit(1);
-        if (c == '\n') {
-            nlines--;
-            if (nlines == 0) return;
-        }
-    }
+    exit(EXIT_SUCCESS);
 }
